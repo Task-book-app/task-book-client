2ef771f0ab464e6cd8539b10914c505cc4101458
Habid B Espinosa <hab.bad@hotmail.com>
2024-07-07 22:33:09 +0200

    hero page with LaziLoadImage component implented, dark modus image still in process, aspect-ratio propertie in css container added

--- src/Components/containers/home/header/HeaderImageContainer.js
+++ src/Components/containers/home/header/HeaderImageContainer.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { useFela } from "react-fela";
-import LazyImage from "../../../presentational/LazyImage";
-import placeholder from "../../../../images/main-dashboard/placeholder-main-dashboard-100w.webp";
 import smallMobile from "../../../../images/hero/600-hero/small-600-hero.webp";
 import largeMobile from "../../../../images/hero/600-hero/600-rsz_dashboard-main.jpg";
 
 import smallTablet from "../../../../images/hero/900-hero/small-900-hero.webp";
 import largeTablet from "../../../../images/hero/900-hero/900-rsz_1dashboard-main.jpg";
 
-import smallDesktop from "../../../../images/main-dashboard/desktop-main-dashboard/small1000.webp";
-import largeDesktop from "../../../../images/main-dashboard/desktop-main-dashboard/large-dashboard-main-grande-original.png";
+import smallDesktop from "../../../../images/hero/1000-hero/small1000.webp";
+import largeDesktop from "../../../../images/hero/1000-hero/large1000.png";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+import "react-lazy-load-image-component/src/effects/blur.css";
+import useMediaQuery from "../../../hooks/useMediaQuery";
 
 const HeaderImageContainer = ({ modus }) => {
+  const max600 = useMediaQuery("(max-width: 600px)");
+  const max900 = useMediaQuery("(min-width: 600px) and (max-width: 900px)");
+
   const {
     css,
     theme: { darkModusBoxes },
   } = useFela();
 
   const rules = () => ({
-    width: "auto",
-    height: "fit-content",
+    width: "100%",
+    aspectRatio: max600 ? "300/203" : max900 ? "300/203" : "244/165",
     padding: "2rem",
     ...darkModusBoxes(modus),
     borderRadius: "2rem",
@@ -30,17 +35,43 @@ const HeaderImageContainer = ({ modus }) => {
 
   return (
     <div className={css(rules)}>
-      <LazyImage
-        placeholderSrc={placeholder} // Placeholder inicial
-        tabletPlaceholder={smallTablet} // Placeholder para tablet
-        mobilePlaceholder={smallMobile} // Placeholder para móvil
-        desktopPlaceholder={smallDesktop} // Placeholder para desktop
-        mobileSrcSet={largeMobile} // Imagen de alta calidad para móvil
-        tabletSrcSet={largeTablet} // Imagen de alta calidad para tablet
-        desktopSrcSet={largeDesktop} // Imagen de alta calidad para desktop
-        alt="Descripción de la imagen"
-        className="responsive-img"
-      />
+      {max600 ? (
+        <LazyLoadImage
+          alt="Dashboard once a user creates an account"
+          width="100%"
+          height="100%"
+          placeholderSrc={modus === "light" ? "" : smallMobile}
+          src={modus === "light" ? "" : largeMobile}
+        />
+      ) : max900 ? (
+        <LazyLoadImage
+          alt="Dashboard once a user creates an account"
+          width="100%"
+          height="100%"
+          placeholderSrc={modus === "light" ? "" : smallTablet}
+          src={modus === "light" ? "" : largeTablet}
+        />
+      ) : modus === "light" ? (
+        <LazyLoadImage
+          alt="Dashboard once a user creates an account"
+          width="100%"
+          height="100%"
+          effect="blur"
+          placeholderSrc={smallDesktop}
+          src={largeDesktop}
+          wrapperClassName="raper-span-lazy"
+        />
+      ) : (
+        <LazyLoadImage
+          alt="Dashboard once a user creates an account"
+          width="100%"
+          height="100%"
+          effect="blur"
+          placeholderSrc={smallDesktop}
+          src={largeDesktop}
+          wrapperClassName="raper-span-lazy"
+        />
+      )}
     </div>
   );
 };
