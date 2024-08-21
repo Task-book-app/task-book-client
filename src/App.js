5e87fd2732dcaabc9e02f40f77a9ff274d4a1eca
Habid B Espinosa <hab.bad@hotmail.com>
2024-08-21 12:50:07 +0200

    set alert message error for logout in global context

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import { HelmetProvider } from "react-helmet-async";
+import { GlobalContext } from "./context/GlobalContext";
 
-const router = createBrowserRouter([{ path: "*", element: <Root /> }]);
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: (
+      <>
+        <GlobalContext>
+          <Root />
+        </GlobalContext>
+      </>
+    ),
+  },
+]);
 
 const App = () => {
   return (
